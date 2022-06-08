3b64f89cd3791476b520546476d02a32bce7332a
Arup Deb <arupdeb20010@gmail.com>
2022-06-08 11:03:30 +0530

    Add files via upload

---
+++ 01 Nearly Solved.cpp
@@ -0,0 +1,68 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to return the sorted array.
+    vector <int> nearlySorted(int arr[], int num, int K){
+ /*
+ //////////////////Using sort STL///////////
+        sort(arr,arr+num);
+ vector<int>ans;
+ for(int i=0;i<num;i++)
+ {
+     ans.push_back(arr[i]);
+ }
+    return ans;
+    }
+    */
+    /////////////without sort//////////////
+     map<int, int> mp;
+       
+       for(int i = 0; i < num; i++)
+       {
+           mp[arr[i]] = 1;
+       }
+       
+       int i = 0;
+       vector<int> ans(num);
+       for(auto x: mp)
+       {
+           ans[i++] = x.first;
+       }
+       return ans;
+    }
+};
+
+
+// { Driver Code Starts.
+
+int main()
+ {
+	int T;
+	cin>> T;
+	
+	while (T--)
+	{
+	    int num, K;
+	    cin>>num>>K;
+	    
+	    int arr[num];
+	    for(int i = 0; i<num; ++i){
+	        cin>>arr[i];
+	    }
+	    Solution ob;
+	    vector <int> res = ob.nearlySorted(arr, num, K);
+	    for (int i = 0; i < res.size (); i++)
+	        cout << res[i] << " ";
+	        
+	    cout<<endl;
+	}
+	
+	return 0;
+}
+  // } Driver Code Ends
