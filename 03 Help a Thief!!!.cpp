25825438ee832a1b8ac7c687647580a78e7f89c2
Arup Deb <arupdeb20010@gmail.com>
2022-06-10 14:23:29 +0530

    Add files via upload

---
+++ 03 Help a Thief!!!.cpp
@@ -0,0 +1,75 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    int maxCoins(int A[], int B[], int T, int N)
+    {
+        /*
+    int sum=0;
+    //while(T>0)
+    //{
+        if(T==0)
+        {
+            return 0;
+        }
+        else{
+            for(int i=0;i<N;i++)
+            {
+                if(A[i]<=T){
+                sum+=A[i]*B[i];
+                T=T-A[i];
+                }
+            }
+        }
+    //}
+    return sum;
+    */
+       vector<pair<int,int>>v;
+       for(int i=0;i<N;i++){
+           v.push_back({B[i],A[i]});
+       }
+       int ans=0;
+       sort(v.begin(),v.end());
+       for(int i=v.size()-1;i>=0;i--){
+           int a=v[i].second;
+           int b=v[i].first;
+           if(T>=a){
+                T-=a;
+                ans+=(a*b);
+           }else if(T<a or T==0){
+               ans+=(T*b);
+               break;
+           }
+       }
+       return ans;
+        
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int T,N;
+        cin>>T>>N;
+        
+        int A[N], B[N];
+        
+        for(int i=0; i<N; i++)
+            cin>>A[i];
+        for(int i=0; i<N; i++)
+            cin>>B[i];
+
+        Solution ob;
+        cout << ob.maxCoins(A,B,T,N) << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
